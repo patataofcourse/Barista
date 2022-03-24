29d7fb4dc3fe6145492e450b356e9c31447a37ee
patataofcourse <patataofcourse@gmail.com>
2022-03-24 17:51:51 +0100

    prepare libcitro2d stuff

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,28 @@ use ctru::console::Console;
 use ctru::services::apt::Apt;
 use ctru::services::hid::{Hid, KeyPad};
 
+use libctru::{
+    C2D_Sprite as Sprite,
+    C2D_DEFAULT_MAX_OBJECTS,
+    C3D_RenderTarget,
+    C3D_DEFAULT_CMDBUF_SIZE,
+    GFX_TOP,
+    GFX_LEFT,
+};
+
 fn main() {
     let apt = Apt::init().unwrap();
     let hid = Hid::init().unwrap();
     let gfx = Gfx::default();
     let console = Console::init(Screen::Bottom);
     console.select();
+    let screen: *mut C3D_RenderTarget;
+    unsafe {
+        libctru::C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+        libctru::C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+        libctru::C2D_Prepare();
+        screen = libctru::C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+    }
 
     println!("Welcome to Barista!");
     println!("\x1b[4;8H:)");
