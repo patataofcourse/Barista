b9b8d46af9a53ca783dfac5fa3bcf936f3cee8e0
patataofcourse <patataofcourse@gmail.com>
2022-05-25 17:11:51 +0200

    Add the rest of the objects to the new scene thing

--- src/scene/top_screen.rs
+++ src/scene/top_screen.rs
@@ -1,5 +1,5 @@
 use ctru::gfx::Screen;
-use std::collections::HashMap;
+use std::{collections::HashMap, iter::FromIterator};
 use ui_lib::{BaristaUI, Image, Object, Scene, SpriteSheet, StaticObject};
 
 pub struct BaristaSprites {
@@ -12,15 +12,43 @@ impl Object for BaristaSprites {
     fn draw(&self) -> bool {
         let img = match self.images.get(self.cur_image) {
             Some(c) => c,
-            None => return true,
+            None => return false,
         };
         img.2.draw(img.0, img.1, 1.0, 1.0, 0.0, self.depth)
     }
 }
 
 impl BaristaSprites {
-    fn switch(&mut self, img: &'static str) {
+    pub fn switch(&mut self, img: &'static str) {
         self.cur_image = img;
+    }
+}
+
+pub struct Sign {
+    pub x: u16,
+    pub y: u16,
+    pub sign_image: Image,
+    pub sign_text: HashMap<String, Image>,
+    pub cur_text: &'static str,
+    pub depth: f32,
+}
+
+impl Object for Sign {
+    fn draw(&self) -> bool {
+        let text = match self.sign_text.get(self.cur_text) {
+            Some(c) => c,
+            None => return false,
+        };
+
+        self.sign_image
+            .draw(self.x, self.y, 1.0, 1.0, 0.0, self.depth)
+            && text.draw(self.x, self.y, 1.0, 1.0, 0.0, self.depth)
+    }
+}
+
+impl Sign {
+    pub fn switch(&mut self, text: &'static str) {
+        self.cur_text = text;
     }
 }
 
@@ -34,10 +62,22 @@ pub fn top_screen_scene<'a>(ui: &BaristaUI) -> Scene<'a> {
     let mut scene = Scene::new(ui, Screen::Top, Some(bg_sheet.get_sprite(0).unwrap()));
 
     // Barista / Nicole
-    let barista = barista_sheet.get_sprite(0).unwrap();
-    let nicole = barista_sheet.get_sprite(1).unwrap();
+    scene.add_object(BaristaSprites {
+        depth: 0.0,
+        images: HashMap::from_iter(vec![
+            (
+                "barista".to_string(),
+                (255, 70, barista_sheet.get_sprite(0).unwrap()),
+            ),
+            (
+                "nicole".to_string(),
+                (174, 17, barista_sheet.get_sprite(1).unwrap()),
+            ),
+        ]),
+        cur_image: "nicole",
+    });
 
-    // Foreground
+    // Foreground (counter)
     scene.add_object(StaticObject {
         x: 0,
         y: 188,
@@ -49,8 +89,17 @@ pub fn top_screen_scene<'a>(ui: &BaristaUI) -> Scene<'a> {
     });
 
     // Sign
-    let sign = sign_sheet.get_sprite(0).unwrap();
-    let sign_text = sign_sheet.get_sprite(1).unwrap();
+    scene.add_object(Sign {
+        x: 30,
+        y: 150,
+        sign_image: sign_sheet.get_sprite(0).unwrap(),
+        sign_text: HashMap::from_iter(vec![(
+            "opening".to_string(),
+            sign_sheet.get_sprite(1).unwrap(),
+        )]),
+        depth: 0.0,
+        cur_text: "opening",
+    });
 
     scene
 }
